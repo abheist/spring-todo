f81596445ec0211b66d49c2c1f5dcee238e98ab0
abheist <abhishek291093@gmail.com>
2024-09-30 18:29:29 -0300

    first commit

---
+++ src/test/java/abheist/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package abheist.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
